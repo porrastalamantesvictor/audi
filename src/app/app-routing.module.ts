2ef52ececc865c97fe47c912f8127539b950e193
victor porras <porrastalamantesvictor@gmail.com>
2019-05-21 17:28:46 -0600

    feat(index):se agrega link boostrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ModelsComponent } from './models/models.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+{ path: 'home',component:HomeComponent},
+{path: 'models',component:ModelsComponent},
+{path:'services', component:ServicesComponent},
+{path: '**', component:HomeComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
